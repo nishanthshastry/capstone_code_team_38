669453e66ff8ae4dd83e6907b39cd547f12c1a0f
nishanthshastry <nshastry00@gmail.com>
2021-12-07 13:37:05 +0530

    json_changed_operation

--- docs/CNN_OS.py
+++ docs/CNN_OS.py
@@ -7,7 +7,8 @@ for i in range(1, procs+1):
     proc_1.append(i)
 
 arr_1 = [1,1,2,3,4,5,6,7,8,9,10,11]
-burt_1 = [3,2,1,3,2,3,2,1,3,3,2,2]
+# burt_1 = [3,2,1,3,2,3,2,1,3,3,2,2]
+burt_1 = [3,2,1,3,2,5,2,1,1,6,2,2]
 prt_1 = [3,2,1,5,4,6,8,9,7,10,11,12]
 
 dfa = pd.DataFrame(list(zip(proc_1, arr_1, burt_1, prt_1)), columns =['process_id', 'arrival', 'burst', 'priority'], index=None)
